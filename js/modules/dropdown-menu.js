d2a8b35e77151401c872508dade56afa8a219b75
Matheus Rodrigues de Mello <matheus_usagui@hotmail.com>
2022-12-07 18:55:15 -0300

    bug-fix

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -5,31 +5,31 @@ export default class DropDownMenu {
     this.dropdownMenus = document.querySelectorAll(target);
     this.activeClass = classe;
 
-    // Define argumentos (["touchstart", "click"]) se o parâmetro events não for definido
+// Define argumentos (["touchstart", "click"]) se o parâmetro events não for definido
     if (events === undefined) this.eventsList = ["touchstart", "click"];
     else this.eventsList = events;
-    
+
     this.showMenu = this.showMenu.bind(this);
   }
 
   // Ativa o dropdownmenu e adiciona a função que observa o click fora 
   showMenu(event) {
-    event.preventDefault();
-    const element = event.currentTarget;
+event.preventDefault();
+const element = event.currentTarget;
     element.classList.toggle(this.activeClass);
     outsideClick(element, this.eventsList, () => {
       element.classList.remove(this.activeClass);
     });
   }
 
-  // Adiciona os eventos ao dropdown menu
+// Adiciona os eventos ao dropdown menu
   addDropDownMenusEvent() {
     this.dropdownMenus.forEach((menu) => {
       this.eventsList.forEach((userEvent) => {
         menu.addEventListener(userEvent, this.showMenu);
       });
     });
-  }
+}
 
   init() {
     if (this.dropdownMenus.length) {
