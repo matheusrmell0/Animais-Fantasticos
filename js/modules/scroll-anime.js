a636caf936423031954dd54403db9c6bdde00f4c
Matheus Rodrigues de Mello <matheus_usagui@hotmail.com>
2022-12-07 15:05:08 -0300

    Animacao no scroll refatorada

--- js/modules/scroll-anime.js
+++ js/modules/scroll-anime.js
@@ -5,25 +5,42 @@ export default class ScrollAnime {
     this.classe = classe;
     this.halfwindow = window.innerHeight * 0.65;
 
-    this.scrollAnimation = this.scrollAnimation.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  scrollAnimation() {
-    this.sections.forEach((item) => {
-    const isSectionVisible =
-      item.getBoundingClientRect().top - this.halfwindow < 0;
-    if (isSectionVisible) {
-      item.classList.add(this.classe);
-    } else if (item.classList.contains(this.classe)) {
-      item.classList.remove(this.classe);
-    }
-  });
-}
+  // Pega a distância de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfwindow),
+      };
+    });
+  }
+
+  // Verifica a distância de cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add(this.classe);
+      } else if (item.element.classList.contains(this.classe)) {
+        item.element.classList.remove(this.classe);
+      }
+    });
+  }
 
   init() {
     if (this.sections.length) {
-      this.scrollAnimation();
-      document.addEventListener("scroll", this.scrollAnimation);
+      this.getDistance();
+      this.checkDistance();
+      document.addEventListener("scroll", this.checkDistance);
     }
+    return this;
+  }
+
+  // Revemo o evento de scroll
+  destroy() {
+    document.removeEventListener("scroll", this.checkDistance);
   }
 }
