8c6b2f6c7fcf87905e7bbd8aae4ba4241023a0d6
Matheus Rodrigues de Mello <matheus_usagui@hotmail.com>
2022-12-07 21:33:45 -0300

    Animais JSON Bug Fix

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -2,7 +2,7 @@
 import AnimeNumbers from "./helper/anime-numbers.js";
 
 export default function fetchAnimals(target, url) {
-if(url === undefined) url =  '../json/animaisapi.json'
+if(url === undefined) url =  './animaisapi.json'
 
   // Cria a div contendo informações com o total de animais
   function createAnimal(animal) {
